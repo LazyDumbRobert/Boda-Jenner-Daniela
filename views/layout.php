3ffea475ea505d8413b63a4731d4873b6abf3f10
Roberto Santizo <robertsantizo76@gmail.com>
2024-10-17 22:48:57 -0600

    cambios font 5

--- views/layout.php
+++ views/layout.php
@@ -31,13 +31,13 @@
             </p>
 
             <div class="nombres__container">
-            <p class="nombres__texto elegant_font">
+            <p class="nombres__texto parisienne-regular">
                     <span style="position: absolute; left: -9999px;">J</span>Jenner
                 </p>
-                <p class="nombres__texto elegant_font">
+                <p class="nombres__texto parisienne-regular">
                     <span style="position: absolute; left: -9999px;">y</span>y
                 </p>
-                <p class="nombres__texto elegant_font">Daniela</p>
+                <p class="nombres__texto parisienne-regular">Daniela</p>
             </div>
         </div>
 
